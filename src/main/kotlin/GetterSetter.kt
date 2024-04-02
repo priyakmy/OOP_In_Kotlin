03b786621ba99b230d81fe7c6ffe3e6c29dee5d3
priya Kumari <priyakum1071@gmail.com>
2024-04-03 01:12:42 +0530

    OOPs

---
+++ src/main/kotlin/GetterSetter.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val book = Book()
+    print(book.author)
+
+}
+class Book() {
+    var author: String = "Denish Ritchi"
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+}
