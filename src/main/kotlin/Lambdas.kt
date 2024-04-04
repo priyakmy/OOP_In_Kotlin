b695c55f5f169d7660107dce6269fe5b44cf8dc3
priya Kumari <priyakum1071@gmail.com>
2024-04-04 14:18:19 +0530

    OOPs

---
+++ src/main/kotlin/Lambdas.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val product = {A:Int,B:Int -> A*B }
+    val greeting = { println("This is Lambdas Function!")}
+    val result = product(4,5,)
+
+    greeting()
+
+    println(result)
+}
